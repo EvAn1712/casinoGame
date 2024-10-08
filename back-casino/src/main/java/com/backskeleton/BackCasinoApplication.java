015a8907283f927f11d01ef701c1fd8c1970f972
cleonard096 <cervantesleonard096@gmail.com>
2024-10-08 11:09:20 +0200

    first commit v2

---
+++ back-casino/src/main/java/com/backskeleton/BackCasinoApplication.java
@@ -0,0 +1,13 @@
+package com.takima.backskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackCasinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(com.takima.backskeleton.BackCasinoApplication.class, args);
+	}
+
+}
---
+++ back-casino/src/test/java/com/epf/backcasino/BackCasinoApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.epf.backcasino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackCasinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
