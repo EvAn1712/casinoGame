e8101f5505585fe46b41eb853b7a292b0ed13c73
Evan <evan.chinnaya@epfedu.fr>
2024-11-13 13:05:31 +0100

    Test 5 GameController

--- back-casino/src/main/java/com/backcasino/controller/GameController.java
+++ back-casino/src/main/java/com/backcasino/controller/GameController.java
@@ -28,47 +28,63 @@ public class GameController {
     @Autowired
     private PlayerService playerService;
 
-    GameDTO gameDTO;
-
     @PostMapping("/create")
     public ResponseEntity<GameDTO> createGame(@RequestBody GameCreationRequest request) {
         Game game = gameService.createGame(request.getPlayerId(), request.getBetAmount());
-        gameDTO = new GameDTO(game);
-        return ResponseEntity.ok(gameDTO);
+        GameDTO gamedto = new GameDTO(game);
+        return ResponseEntity.ok(gamedto);
     }
 
     @PostMapping("/hit")
-    public ResponseEntity<GameDTO> playerHit() {
-        Game game = gameService.findById(gameDTO.getGameId());
-        game.setDeck(gameDTO.getDeck());
-        game.setPlayerHand(gameDTO.getPlayerHand());
-        game.setPlayerScore(gameDTO.getPlayerScore());
-        game.setDealerHand(gameDTO.getDealerHand());
-        game.setDealerScore(gameDTO.getDealerScore());
+    public ResponseEntity<GameDTO> playerHit(@RequestBody GameDTO request) {
+        Game game = gameService.findById(request.getGameId());
+        game.setPlayerHand(request.getPlayerHand());
+        game.setDealerHand(request.getDealerHand());
+        game.setPlayerScore(request.getPlayerScore());
+        game.setDealerScore(request.getDealerScore());
+        game.setDeck(request.getDeck());
         gameService.playerHit(game);
-        gameDTO = new GameDTO(game);
-        return ResponseEntity.ok(gameDTO);
+        GameDTO gamedto = new GameDTO(game);
+        return ResponseEntity.ok(gamedto);
     }
 
     @PostMapping("/stand")
-    public ResponseEntity<GameDTO> playerStand(@RequestBody GameActionRequestDTO request) {
+    public ResponseEntity<GameDTO> playerStand(@RequestBody GameDTO request) {
         Game game = gameService.findById(request.getGameId());
+        game.setPlayerHand(request.getPlayerHand());
+        game.setDealerHand(request.getDealerHand());
+        game.setPlayerScore(request.getPlayerScore());
+        game.setDealerScore(request.getDealerScore());
+        game.setDeck(request.getDeck());
         gameService.playerStand(game);
-        return ResponseEntity.ok(new GameDTO(game));
+        GameDTO gamedto = new GameDTO(game);
+        return ResponseEntity.ok(gamedto);
     }
 
     @PostMapping("/surrender")
-    public ResponseEntity<GameDTO> playerSurrender(@RequestBody GameEndRequestDTO request) {
+    public ResponseEntity<GameDTO> playerSurrender(@RequestBody GameDTO request) {
         Game game = gameService.findById(request.getGameId());
+        game.setPlayerHand(request.getPlayerHand());
+        game.setDealerHand(request.getDealerHand());
+        game.setPlayerScore(request.getPlayerScore());
+        game.setDealerScore(request.getDealerScore());
+        game.setDeck(request.getDeck());
         gameService.playerSurrender(game);
-        return ResponseEntity.ok(new GameDTO(game));
+        GameDTO gamedto = new GameDTO(game);
+        return ResponseEntity.ok(gamedto);
     }
 
     @PostMapping("/double")
-    public ResponseEntity<GameDTO> playerDouble(@RequestBody GameActionRequestDTO request) {
+    public ResponseEntity<GameDTO> playerDouble(@RequestBody GameDTO request) {
         Game game = gameService.findById(request.getGameId());
+        game.setPlayerHand(request.getPlayerHand());
+        game.setDealerHand(request.getDealerHand());
+        game.setPlayerScore(request.getPlayerScore());
+        game.setDealerScore(request.getDealerScore());
+        game.setDeck(request.getDeck());
         gameService.playerDouble(game);
-        return ResponseEntity.ok(new GameDTO(game));
+        GameDTO gamedto = new GameDTO(game);
+        return ResponseEntity.ok(gamedto);
     }
 
 }
